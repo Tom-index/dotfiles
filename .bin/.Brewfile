1c6f07b017f50325f8bf7925e791abf94c060d9c
Tom-index <150645282+Tom-index@users.noreply.github.com>
2023-12-05 08:41:33 +0900

    update

--- .bin/.Brewfile
+++ .bin/.Brewfile
@@ -4,14 +4,20 @@ tap "homebrew/bundle"
 tap "teamookla/speedtest"
 
 # brews
+brew "git"
 brew "neofetch"
 brew "peco"
 brew "sl"
 brew "tree"
-brew "zsh"
 brew "teamookla/speedtest/speedtest"
 
 # casks
+cask "brave-browser"
+cask "discord"
+cask "google-chrome"
+cask "intellij-idea"
+cask "iterm2"
+cask "notion"
 cask "visual-studio-code"
 
 # vscode extensions
--- .bin/brew.sh
+++ .bin/brew.sh
@@ -5,4 +5,25 @@ if [ "$(uname)" != "Darwin" ] ; then
 	exit 1
 fi
 
+# if you use "mas" command in .Brewfile, add below script.
+# function login_check {
+#   sleep 1; echo "Open the App Store." && open -a App\ Store
+#   while true; do
+#     echo -n "$* [Y/n]: (default: n) "
+#     read ANS
+#     case $ANS in
+#       [Yy]*)
+#         return 0
+#         ;;
+#       *)
+#         open_app_store
+#         ;;
+#     esac
+#   done
+# }
+#
+# if login_check "Did you login?"; then
+#   brew bundle --global
+# fi
+
 brew bundle --global
